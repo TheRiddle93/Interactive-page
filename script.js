d115b35f150ac192d532c4e262bc61a227ab7327
TheRiddle93 <34629376+TheRiddle93@users.noreply.github.com>
2021-02-18 00:23:52 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
-function sayHi() {
-  var txtName = document.getElementById("txtName");
-  var txtOutput = document.getElementById("txtOutput");
-  var name = txtName.value;
-  txtOutput.value = "Hi there, " + name + "!";
-}
+var btn = document.getElementById("btn");
+var txtInput = document.getElementById("txtInput");
+var txtOutput = document.getElementById("txtOutput");
+var input = txtInput.value;
+/*
+function sayHi(hello) {
+  txtInput = document.getElementById("txtInput");
+  txtOutput = document.getElementById("txtOutput");
+  input = txtInput.value;
+  txtOutput.value = "Hi there, " + hello + "!";
+}*/
+
+const hey = function (hello) {
+  return "Hello there " + hello;
+};
+
+btn.addEventListener("click", function () {
+  txtInput = document.getElementById("txtInput");
+  input = txtInput.value;
+  txtOutput.value = hey(input);
+});
